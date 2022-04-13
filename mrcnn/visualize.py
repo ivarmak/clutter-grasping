19c91f7fd1af8e11780b7425f59abad85ec07e5d
ivarmak <ivarmak@hotmail.com>
2022-04-13 16:44:17 +0200

    changed font colour mrcnn predictions

--- mrcnn/visualize.py
+++ mrcnn/visualize.py
@@ -144,7 +144,7 @@ def display_instances(image, boxes, masks, class_ids, class_names,
         else:
             caption = captions[i]
         ax.text(x1, y1 + 8, caption,
-                color='w', size=11, backgroundcolor="none")
+                color='black', size=11, backgroundcolor="none")
 
         # Mask
         mask = masks[:, :, i]
