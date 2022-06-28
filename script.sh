3bebc7e34b7185c4eb9380fda2e9e318bb059c02
ivarmak <ivarmak@hotmail.com>
2022-06-29 00:57:23 +0200

    result processing

--- script.sh
+++ script.sh
@@ -1,8 +1,8 @@
 #/home/ivar/.pyenv/shims/python3
 
-python3 simulation.py pack
-python3 simulation.py pack
-python3 simulation.py clutter
-python3 simulation.py clutter
-python3 simulation.py clutter
-python3 simulation.py pack
+python3 simulation.py iso
+python3 simulation.py iso
+python3 simulation.py iso
+python3 simulation.py pile
+python3 simulation.py pile
+python3 simulation.py pile
