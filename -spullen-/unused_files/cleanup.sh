8017232c2405eccf283a49eebee4b801ffcac477
ivarmak <ivarmak@hotmail.com>
2022-08-10 16:29:40 +0200

    rm redundant files

--- -spullen-/unused_files/cleanup.sh
+++
@@ -1,2 +0,0 @@
-rm -r results/2021*
-rm network_output/*.png
--- script.sh
+++
@@ -1,9 +0,0 @@
-#/home/ivar/.pyenv/shims/python3
-
-python3 simulation.py clutter
-python3 simulation.py clutter
-python3 simulation.py clutter
-python3 simulation.py clutter
-python3 simulation.py clutter
-python3 simulation.py pile
-python3 simulation.py pile
